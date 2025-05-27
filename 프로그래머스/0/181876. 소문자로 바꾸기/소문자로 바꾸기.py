c382b24472e10b7686e447e3e44f9a90d330e95f
Jiyun <162008947+ImJiyun@users.noreply.github.com>
2025-05-27 10:49:00 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.py
@@ -0,0 +1,8 @@
+def solution(myString):
+    # answer = ''
+    # for str in myString:
+    #     if str.isupper():
+    #         answer += str.lower()
+    #     else:
+    #         answer += str
+    return myString.lower()
