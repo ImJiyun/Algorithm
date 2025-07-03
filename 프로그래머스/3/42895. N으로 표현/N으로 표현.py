9d54a1caed0e5ff4aa41be4447d51b268f3640c5
Jiyun <162008947+ImJiyun@users.noreply.github.com>
2025-07-03 15:19:34 +0900

    [level 3] Title: N으로 표현, Time: 16.76 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,20 +1,3 @@
 def solution(N, number):
-    min_val = float('inf')
-    dp = [set() for _ in range(9)] 
-    
-    for i in range(1, 9):
-        dp[i].add(int(str(N) * i))
-        
-        for j in range(1, i):
-            for op1 in dp[j]:
-                for op2 in dp[i-j]:
-                    dp[i].add(op1+op2)
-                    dp[i].add(op1-op2)
-                    dp[i].add(op1*op2)
-                    if op2 != 0:
-                        dp[i].add(op1 // op2)
-            
-        if number in dp[i]:
-            return i
-            
-    return -1
+    answer = 0
+    return answer
