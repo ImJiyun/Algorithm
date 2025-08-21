b0d290095e4209722737e82df2caded8d5498121
Jiyun <162008947+ImJiyun@users.noreply.github.com>
2025-08-21 14:34:52 +0900

    [Gold V] Title: 회의실 배정, Time: 276 ms, Memory: 60360 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+arr.sort(key=lambda x: (x[1], x[0]))
+
+cnt = 0
+end_time = 0
+
+for start, end in arr:
+    if start >= end_time:
+        cnt += 1
+        end_time = end
+           
+print(cnt)        
