a340bfc4f213fced5e2303ab71c70ef34cc9fa33
Jiyun <162008947+ImJiyun@users.noreply.github.com>
2025-09-15 15:04:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 63.58 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -3,12 +3,10 @@ from collections import defaultdict
 def solution(participant, completion):
     d = defaultdict(int)
     
-    for person in participant:
-        d[person] += 1
-    
-    for person in completion:
-        d[person] -= 1
-        if d[person] == 0:
-            del d[person]
-            
-    return next(iter(d))
+    for p in participant:
+        d[p] += 1
+    for p in completion:
+        d[p] -= 1
+        if d[p] == 0:
+            del d[p]
+    return list(d.keys())[0] 
