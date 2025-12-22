5a2a4badd9ad8e1fdbc44ab64440890415b76c92
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2025-12-22 12:05:26 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,38 +1,67 @@
 -- 코드를 입력하세요
-WITH CARS AS (
+# WITH CARS AS (
+#     SELECT
+#         CAR_ID
+#     FROM
+#         CAR_RENTAL_COMPANY_RENTAL_HISTORY
+#     WHERE
+#         START_DATE >= '2022-08-01' 
+#         AND START_DATE <='2022-10-31'
+#     GROUP BY 
+#         CAR_ID
+#     HAVING
+#         COUNT(CAR_ID) >= 5
+# )  
+
+# SELECT
+#     MONTH(START_DATE) AS MONTH,
+#     t1.CAR_ID,
+#     COUNT(*) AS RECORDS
+# FROM
+#     CAR_RENTAL_COMPANY_RENTAL_HISTORY AS t1
+# JOIN
+#     CARS AS t2
+#     ON t1.CAR_ID = t2.CAR_ID
+# WHERE
+#     t1.START_DATE >= '2022-08-01'
+#     AND t1.START_DATE <='2022-10-31'
+# GROUP BY
+#     MONTH,
+#     t1.CAR_ID
+# HAVING
+#     RECORDS != 0
+# ORDER BY
+#     MONTH,
+#     t1.CAR_ID DESC
+
+WITH base AS (
     SELECT
-        CAR_ID
+        MONTH(START_DATE) AS MONTH,
+        CAR_ID,
+        COUNT(*) AS RECORDS
     FROM
         CAR_RENTAL_COMPANY_RENTAL_HISTORY
     WHERE
-        START_DATE >= '2022-08-01' 
-        AND START_DATE <='2022-10-31'
-    GROUP BY 
+        START_DATE BETWEEN '2022-08-01' AND '2022-10-31'
+    GROUP BY
+        MONTH,
         CAR_ID
-    HAVING
-        COUNT(CAR_ID) >= 5
-)  
+), rent_total AS (
+    SELECT
+        *,
+        SUM(RECORDS) OVER (PARTITION BY CAR_ID) AS total
+    FROM
+        base
+)
 
 SELECT
-    MONTH(START_DATE) AS MONTH,
-    t1.CAR_ID,
-    COUNT(*) AS RECORDS
-FROM
-    CAR_RENTAL_COMPANY_RENTAL_HISTORY AS t1
-JOIN
-    CARS AS t2
-    ON t1.CAR_ID = t2.CAR_ID
-WHERE
-    t1.START_DATE >= '2022-08-01'
-    AND t1.START_DATE <='2022-10-31'
-GROUP BY
     MONTH,
-    t1.CAR_ID
-HAVING
-    RECORDS != 0
+    CAR_ID,
+    RECORDS
+FROM
+    rent_total
+WHERE
+    total >= 5
 ORDER BY
     MONTH,
-    t1.CAR_ID DESC
-    
-    
-    
+    CAR_ID DESC
