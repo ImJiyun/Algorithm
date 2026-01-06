8544248d945c922e11114bde95cca3cb066e2bfe
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-01-06 19:37:51 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,18 +1,49 @@
 -- 코드를 작성해주세요
-SELECT 
+# 첫 시도 
+# WITH longer_than_33 AS (
+#     SELECT
+#         FISH_TYPE
+#     FROM
+#         FISH_INFO
+#     GROUP BY
+#         FISH_TYPE
+#     HAVING  
+#         AVG(LENGTH) >= 33
+# )
+
+# SELECT
+#     COUNT(FISH_TYPE) AS FISH_COUNT,
+#     MAX(LENGTH) AS MAX_LENGTH,
+#     FISH_TYPE
+# FROM
+#     FISH_INFO
+# WHERE
+#     FISH_TYPE IN (
+#         SELECT
+#             *
+#         FROM
+#             longer_than_33
+#     )
+# GROUP BY
+#     FISH_TYPE
+
+WITH not_null_table AS(
+    SELECT
+        FISH_TYPE,
+        IF(LENGTH IS NULL, 10, LENGTH) AS LENGTH
+    FROM
+        FISH_INFO
+)
+
+SELECT
     COUNT(FISH_TYPE) AS FISH_COUNT,
     MAX(LENGTH) AS MAX_LENGTH,
     FISH_TYPE
-FROM 
-    (
-        SELECT 
-            FISH_TYPE,
-            IF(LENGTH <= 10 OR LENGTH IS NULL, 10, LENGTH) AS LENGTH
-        FROM FISH_INFO
-    ) AS ADJUSTED_FISH
-GROUP BY 
+FROM
+    not_null_table
+GROUP BY
     FISH_TYPE
-HAVING 
+HAVING
     AVG(LENGTH) >= 33
-ORDER BY 
+ORDER BY
     FISH_TYPE
