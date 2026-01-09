fbba97e1fef31af6100f1b220d07c2b496ff432a
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-01-09 14:48:33 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,21 +1,38 @@
 -- 코드를 입력하세요
-SELECT 
-    MEMBER_NAME, 
-    REVIEW_TEXT, 
-    DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
-FROM 
-    MEMBER_PROFILE t1
-JOIN 
-    REST_REVIEW t2
-ON 
-    t1.MEMBER_ID = t2.MEMBER_ID
-WHERE t1.MEMBER_ID = (
-    SELECT MEMBER_ID
-    FROM 
-        REST_REVIEW 
-    GROUP BY 
-        MEMBER_ID
-    ORDER BY COUNT(*) DESC
-    LIMIT 1
+WITH rank_table AS (
+    SELECT 
+        *,
+        DENSE_RANK() OVER (ORDER BY review_cnt DESC) AS review_rank
+    FROM (
+        SELECT
+            MEMBER_ID,
+            COUNT(MEMBER_ID) AS review_cnt
+        FROM
+            REST_REVIEW 
+        GROUP BY
+            MEMBER_ID
+    ) AS tmp
 )
-ORDER BY REVIEW_DATE, REVIEW_TEXT
+
+SELECT 
+    mp.MEMBER_NAME,
+    r.REVIEW_TEXT,
+    DATE_FORMAT(r.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+FROM
+    MEMBER_PROFILE AS mp
+LEFT JOIN 
+    REST_REVIEW  AS r
+ON 
+    mp.MEMBER_ID = r.MEMBER_ID
+WHERE
+    mp.MEMBER_ID IN (
+        SELECT
+            MEMBER_ID
+        FROM
+            rank_table
+        WHERE
+            review_rank = 1
+    )
+ORDER BY
+    REVIEW_DATE,
+    REVIEW_TEXT
