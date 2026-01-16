b2dbc3bc6328511f673098b481eb91b538677fab
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-01-16 14:25:32 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,17 +1,37 @@
 -- 코드를 입력하세요
+# SELECT
+#     FOOD_TYPE,
+#     REST_ID,
+#     REST_NAME,
+#     FAVORITES
+# FROM (
+#     SELECT
+#         FOOD_TYPE,
+#         REST_ID,
+#         REST_NAME,
+#         FAVORITES,
+#         ROW_NUMBER() OVER (PARTITION BY FOOD_TYPE ORDER BY FAVORITES DESC) AS RN
+#     FROM REST_INFO
+# ) AS Ranked
+# WHERE RN = 1
+# ORDER BY FOOD_TYPE DESC;
+
+WITH fav_rank_table AS (
+    SELECT
+        *,
+        RANK() OVER (PARTITION BY FOOD_TYPE ORDER BY FAVORITES DESC) AS fav_rank
+    FROM
+        REST_INFO 
+)
+
 SELECT
     FOOD_TYPE,
     REST_ID,
     REST_NAME,
     FAVORITES
-FROM (
-    SELECT
-        FOOD_TYPE,
-        REST_ID,
-        REST_NAME,
-        FAVORITES,
-        ROW_NUMBER() OVER (PARTITION BY FOOD_TYPE ORDER BY FAVORITES DESC) AS RN
-    FROM REST_INFO
-) AS Ranked
-WHERE RN = 1
-ORDER BY FOOD_TYPE DESC;
+FROM
+    fav_rank_table
+WHERE
+    fav_rank = 1
+ORDER BY
+    FOOD_TYPE DESC
