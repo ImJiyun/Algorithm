370b934d7781cbf7f0b16fa12365052a4c6dcd54
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-01-20 19:00:17 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,18 +1,50 @@
 -- 코드를 입력하세요
+# SELECT
+#     WRITER_ID,
+#     NICKNAME,
+#     CONCAT(CITY, " ", STREET_ADDRESS1, " ", STREET_ADDRESS2) AS "전체주소",
+#     CONCAT(SUBSTRING(TLNO, 1, 3), "-", SUBSTRING(TLNO, 4, 4), "-", SUBSTRING(TLNO, 8, 4)) AS "전화번호"
+# FROM
+#     USED_GOODS_BOARD t1
+# JOIN 
+#     USED_GOODS_USER t2
+# ON 
+#     t1.WRITER_ID = t2.USER_ID
+# GROUP BY
+#     t1.WRITER_ID
+# HAVING
+#     COUNT(t1.WRITER_ID) >= 3
+# ORDER BY 
+#     USER_ID DESC
+
+WITH order_more_than_3 AS (
+    SELECT
+        WRITER_ID
+    FROM
+        USED_GOODS_BOARD 
+    GROUP BY
+        WRITER_ID
+    HAVING
+        COUNT(WRITER_ID) >= 3
+)
+
 SELECT
-    WRITER_ID,
+    USER_ID,
     NICKNAME,
-    CONCAT(CITY, " ", STREET_ADDRESS1, " ", STREET_ADDRESS2) AS "전체주소",
-    CONCAT(SUBSTRING(TLNO, 1, 3), "-", SUBSTRING(TLNO, 4, 4), "-", SUBSTRING(TLNO, 8, 4)) AS "전화번호"
+    CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS '전체주소',
+    CONCAT(
+        SUBSTRING(TLNO, 1, 3), '-',
+        SUBSTRING(TLNO, 4, 4), '-',
+        SUBSTRING(TLNO, 8, 4)
+    ) AS '전화번호'
 FROM
-    USED_GOODS_BOARD t1
-JOIN 
-    USED_GOODS_USER t2
-ON 
-    t1.WRITER_ID = t2.USER_ID
-GROUP BY
-    t1.WRITER_ID
-HAVING
-    COUNT(t1.WRITER_ID) >= 3
-ORDER BY 
+    USED_GOODS_USER 
+WHERE
+    USER_ID IN (
+        SELECT
+            WRITER_ID
+        FROM
+            order_more_than_3
+)
+ORDER BY
     USER_ID DESC
