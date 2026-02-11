90e93cb9e7760b3d4f88cc9eb02152d4f21553ef
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-02-11 19:43:58 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,7 +1,9 @@
 def solution(my_string):
-    answer = []
-    for char in my_string:
-        if char.isdigit():
-            answer.append(int(char))
-    answer.sort()
-    return answer
+    # answer = []
+    # for char in my_string:
+    #     if char.isdigit():
+    #         answer.append(int(char))
+    # answer.sort()
+    # return answer
+    return sorted(int(char) for char in my_string if char.isdigit())
+
