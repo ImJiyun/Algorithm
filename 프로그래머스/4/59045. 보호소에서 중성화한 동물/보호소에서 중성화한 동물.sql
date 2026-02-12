32cab9e881c9e84a30fda5bd6e76a7c3c9e7462a
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-02-12 10:01:33 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,14 +1,30 @@
 -- 코드를 입력하세요
+# SELECT
+#     i.ANIMAL_ID,
+#     i.ANIMAL_TYPE,
+#     i.NAME
+# FROM 
+#     ANIMAL_INS i
+# JOIN 
+#     ANIMAL_OUTS o
+# ON 
+#     i.ANIMAL_ID = o.ANIMAL_ID
+# WHERE 
+#     SEX_UPON_INTAKE LIKE 'Intact%' AND 
+#     SEX_UPON_OUTCOME NOT LIKE 'Intact%'
+
+# Intact : 중성화 X
 SELECT
-    i.ANIMAL_ID,
-    i.ANIMAL_TYPE,
-    i.NAME
-FROM 
-    ANIMAL_INS i
-JOIN 
-    ANIMAL_OUTS o
-ON 
-    i.ANIMAL_ID = o.ANIMAL_ID
-WHERE 
-    SEX_UPON_INTAKE LIKE 'Intact%' AND 
-    SEX_UPON_OUTCOME NOT LIKE 'Intact%'
+    ai.ANIMAL_ID,
+    ai.ANIMAL_TYPE,
+    ai.NAME
+FROM
+    ANIMAL_INS AS ai
+LEFT JOIN
+    ANIMAL_OUTS AS ao
+ON
+    ai.ANIMAL_ID = ao.ANIMAL_ID
+WHERE
+    ai.SEX_UPON_INTAKE LIKE '%Intact%'
+    AND
+    ao.SEX_UPON_OUTCOME NOT LIKE '%Intact%'
