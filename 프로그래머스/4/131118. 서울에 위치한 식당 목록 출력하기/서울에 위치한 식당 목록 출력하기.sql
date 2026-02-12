64c6081d6c4661aa0740aa1d3304a35859c37a13
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-02-12 10:26:40 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,18 +1,40 @@
 -- 코드를 입력하세요
-SELECT 
-    ri.rest_id, 
-    ri.rest_name, 
-    ri.food_type, 
-    ri.favorites, 
-    ri.address, 
-    ROUND(AVG(rr.review_score), 2) AS score
-FROM 
-    rest_info ri
-JOIN 
-    rest_review rr ON ri.rest_id = rr.rest_id
-WHERE 
-    ri.address LIKE '서울%'
-GROUP BY 
-    ri.rest_id, ri.rest_name, ri.food_type, ri.address
-ORDER BY 
-    score DESC, ri.favorites DESC;
+# SELECT 
+#     ri.rest_id, 
+#     ri.rest_name, 
+#     ri.food_type, 
+#     ri.favorites, 
+#     ri.address, 
+#     ROUND(AVG(rr.review_score), 2) AS score
+# FROM 
+#     rest_info ri
+# JOIN 
+#     rest_review rr ON ri.rest_id = rr.rest_id
+# WHERE 
+#     ri.address LIKE '서울%'
+# GROUP BY 
+#     ri.rest_id, ri.rest_name, ri.food_type, ri.address
+# ORDER BY 
+#     score DESC, ri.favorites DESC;
+
+
+SELECT
+    ri.REST_ID,
+    ri.REST_NAME,
+    ri.FOOD_TYPE,
+    ri.FAVORITES,
+    ri.ADDRESS,
+    ROUND(AVG(rr.REVIEW_SCORE), 2) AS SCORE
+FROM
+    REST_INFO AS ri
+JOIN
+    REST_REVIEW AS rr
+ON
+    ri.REST_ID = rr.REST_ID
+WHERE
+    ADDRESS LIKE "서울%"
+GROUP BY
+    ri.REST_ID
+ORDER BY
+    SCORE DESC,
+    FAVORITES DESC
