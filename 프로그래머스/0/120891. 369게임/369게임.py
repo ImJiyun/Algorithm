ad1093f8ebd0cbee8008ad6dcd6a2d9c5a07ecf5
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-02-25 15:16:33 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -1,9 +1,2 @@
 def solution(order):
-    answer = 0
-    
-    nums = ['3', '6', '9']
-    for ch in str(order):
-        if ch in nums:
-            answer += 1
-        
-    return answer
+    return sum(1 for ch in str(order) if ch in '369')
