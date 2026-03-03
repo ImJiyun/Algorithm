94f9661763d20ab010b882ad64d0ddcea507f6d9
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-03-03 10:16:15 +0900

    [level 2] Title: 타겟 넘버, Time: 319.24 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 361.46 ms
+메모리: 9.27 MB, 시간: 319.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 03일 09:01:55
+2026년 03월 03일 10:16:10
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,38 @@
+# def solution(numbers, target):
+#     cnt = [0]
+    
+#     def dfs(idx, total):
+        
+#         if idx == len(numbers):
+#             if total == target:
+#                 cnt[0] += 1  
+#             return
+        
+#         dfs(idx + 1, total + numbers[idx])
+        
+#         dfs(idx + 1, total - numbers[idx])
+    
+#     dfs(0, 0)
+    
+#     return cnt[0]
+
+
 def solution(numbers, target):
-    cnt = [0]
+    answer = 0
+    
+    length = len(numbers)
     
     def dfs(idx, total):
+        nonlocal answer
         
-        if idx == len(numbers):
+        if idx == length:
             if total == target:
-                cnt[0] += 1  
+                answer += 1
             return
         
-        dfs(idx + 1, total + numbers[idx])
+        dfs(idx+1, total + numbers[idx])
         
-        dfs(idx + 1, total - numbers[idx])
-    
+        dfs(idx+1, total - numbers[idx])
+
     dfs(0, 0)
-    
-    return cnt[0]
+    return answer
