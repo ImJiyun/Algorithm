6192700abd4fed8703e12c314bd558c6bcc5838b
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-03-03 10:56:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.04 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 13.12 ms
+메모리: 9.34 MB, 시간: 12.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 03일 06:47:11
+2026년 03월 03일 10:56:26
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,28 +1,57 @@
+# from collections import deque
+
+# def solution(maps):
+#     dir = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+#     n = len(maps)
+#     m = len(maps[0])
+#     visited = set([(0, 0)])
+    
+#     queue = deque([(0, 0, 1)])
+    
+#     while (len(queue) > 0):
+#         r, c, k = queue.popleft()
+        
+#         if r == n - 1 and c == m - 1:
+#             return k
+        
+#         for dr, dc in dir:
+#             nr = r + dr
+#             nc = c + dc
+            
+#             if nr < 0 or nc < 0 or nr > n-1 or nc > m-1 or (nr, nc) in visited:
+#                 continue
+#             if maps[nr][nc] == 0:
+#                 continue
+#             visited.add((nr, nc))
+#             queue.append((nr, nc, k+1))
+            
+#     return -1
+
 from collections import deque
 
 def solution(maps):
+    n, m = len(maps), len(maps[0])
     dir = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-    n = len(maps)
-    m = len(maps[0])
-    visited = set([(0, 0)])
     
-    queue = deque([(0, 0, 1)])
+    q = deque([(0,0)])
+    visited = [[False] * m for _ in range(n)]
+    visited[0][0] = True
     
-    while (len(queue) > 0):
-        r, c, k = queue.popleft()
+    while True:
+        if len(q) == 0: return -1
         
-        if r == n - 1 and c == m - 1:
-            return k
+        r, c = q.popleft()
+        
+        if r == n-1 and c == m-1: break
         
         for dr, dc in dir:
-            nr = r + dr
-            nc = c + dc
+            nr, nc = r + dr, c + dc
             
-            if nr < 0 or nc < 0 or nr > n-1 or nc > m-1 or (nr, nc) in visited:
+            if nr < 0 or nr >= n or nc < 0 or nc >= m or maps[nr][nc] == 0 or visited[nr][nc]:
                 continue
-            if maps[nr][nc] == 0:
-                continue
-            visited.add((nr, nc))
-            queue.append((nr, nc, k+1))
-            
-    return -1
+            visited[nr][nc] = True
+            maps[nr][nc] += maps[r][c]
+                            
+            q.append((nr, nc))
+    
+    return maps[n-1][m-1] 
