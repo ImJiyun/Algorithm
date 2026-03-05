699098c4972a06326e4d7282f151acd363d7da71
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-03-05 16:17:40 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/README.md
+++ 프로그래머스/0/181879. 길이에 따른 연산/README.md
@@ -1,10 +1,10 @@
 # [level 0] 길이에 따른 연산 - 181879 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181879?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181879) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 0.00 ms
+메모리: 9.19 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 27일 13:28:17
+2026년 03월 05일 16:17:38
 
 ### 문제 설명
 
--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,8 +1,16 @@
 def solution(num_list):
+    # if len(num_list) >= 11:
+    #     answer = sum(num_list)
+    # else:
+    #     answer =  1
+    #     for num in num_list:
+    #         answer *= num
+    # return answer
+    
     if len(num_list) >= 11:
-        answer = sum(num_list)
+        return sum(num_list)
     else:
-        answer =  1
+        answer = 1
         for num in num_list:
             answer *= num
-    return answer
+        return answer
