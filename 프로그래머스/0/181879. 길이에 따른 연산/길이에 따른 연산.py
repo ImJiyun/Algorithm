cbef1f8061f60e49d606f2585a83a44a3429abff
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-03-05 16:49:33 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,16 +1,7 @@
+import math 
+
 def solution(num_list):
-    # if len(num_list) >= 11:
-    #     answer = sum(num_list)
-    # else:
-    #     answer =  1
-    #     for num in num_list:
-    #         answer *= num
-    # return answer
-    
     if len(num_list) >= 11:
         return sum(num_list)
     else:
-        answer = 1
-        for num in num_list:
-            answer *= num
-        return answer
+        return math.prod(num_list)
