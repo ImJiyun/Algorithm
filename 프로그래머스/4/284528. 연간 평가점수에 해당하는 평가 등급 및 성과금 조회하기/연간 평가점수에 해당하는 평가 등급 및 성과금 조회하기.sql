c99d156cb5a2edb1225607f776ad701f0644ec4d
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-03-12 13:42:37 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,31 +1,60 @@
 -- 코드를 작성해주세요
+# SELECT
+#     he.EMP_NO,
+#     he.EMP_NAME,
+#     CASE 
+#         WHEN AVG(SCORE) >= 96 THEN 'S'
+#         WHEN AVG(SCORE) >= 90 THEN 'A'
+#         WHEN AVG(SCORE) >= 80 THEN 'B'
+#         ELSE 'C'
+#     END AS 'GRADE',
+    
+#     CASE
+#         WHEN AVG(SCORE) >= 96 THEN SAL * 0.2
+#         WHEN AVG(SCORE) >= 90 THEN SAL * 0.15
+#         WHEN AVG(SCORE) >= 80 THEN SAL * 0.1
+#         ELSE SAL * 0
+#     END AS 'BONUS'
+# FROM
+#     HR_DEPARTMENT hd
+# JOIN
+#     HR_EMPLOYEES he
+# ON 
+#     hd.DEPT_ID = he.DEPT_ID
+# JOIN 
+#     HR_GRADE hg
+# ON 
+#     he.EMP_NO = hg.EMP_NO
+# GROUP BY
+#     he.EMP_NO, he.EMP_NAME
+# ORDER BY 
+#     he.EMP_NO
+
 SELECT
-    he.EMP_NO,
-    he.EMP_NAME,
+    hg.EMP_NO,
+    hr.EMP_NAME,
+    
     CASE 
-        WHEN AVG(SCORE) >= 96 THEN 'S'
-        WHEN AVG(SCORE) >= 90 THEN 'A'
-        WHEN AVG(SCORE) >= 80 THEN 'B'
+        WHEN AVG(hg.SCORE) >= 96 THEN 'S'
+        WHEN AVG(hg.SCORE) >= 90 THEN 'A'
+        WHEN AVG(hg.SCORE) >= 80 THEN 'B'
         ELSE 'C'
     END AS 'GRADE',
     
-    CASE
-        WHEN AVG(SCORE) >= 96 THEN SAL * 0.2
-        WHEN AVG(SCORE) >= 90 THEN SAL * 0.15
-        WHEN AVG(SCORE) >= 80 THEN SAL * 0.1
-        ELSE SAL * 0
+    CASE 
+        WHEN AVG(hg.SCORE) >= 96 THEN SAL * 0.2
+        WHEN AVG(hg.SCORE) >= 90 THEN SAL * 0.15
+        WHEN AVG(hg.SCORE) >= 80 THEN SAL * 0.1
+        ELSE 0
     END AS 'BONUS'
 FROM
-    HR_DEPARTMENT hd
-JOIN
-    HR_EMPLOYEES he
-ON 
-    hd.DEPT_ID = he.DEPT_ID
-JOIN 
-    HR_GRADE hg
-ON 
-    he.EMP_NO = hg.EMP_NO
+    HR_GRADE AS hg
+LEFT JOIN
+    HR_EMPLOYEES AS hr
+ON
+    hg.EMP_NO = hr.EMP_NO
 GROUP BY
-    he.EMP_NO, he.EMP_NAME
-ORDER BY 
-    he.EMP_NO
+    hg.EMP_NO,
+    hr.EMP_NAME
+ORDER BY
+    hg.EMP_NO
