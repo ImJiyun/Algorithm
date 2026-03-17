44d6612f96ad20eec9fd8709cafae087e930affa
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-03-17 14:51:14 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,8 +1,19 @@
+# def solution(n):
+#     factorial = 1
+#     for i in range(1, n + 1):
+#         factorial *= i
+#         if factorial == n:
+#             return i
+#         if factorial > n:
+#             return i - 1
+
 def solution(n):
     factorial = 1
-    for i in range(1, n + 1):
+    i = 1
+    while True:
         factorial *= i
         if factorial == n:
             return i
         if factorial > n:
             return i - 1
+        i += 1
