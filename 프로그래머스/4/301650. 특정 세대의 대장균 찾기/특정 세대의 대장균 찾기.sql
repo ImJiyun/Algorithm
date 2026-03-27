1285f9654f82c2f8809765440150d5cf8dc59ae0
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-03-27 10:31:41 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,17 +1,50 @@
 -- 코드를 작성해주세요
-SELECT 
-    F.ID
-FROM 
-    ECOLI_DATA F
-JOIN 
-    ECOLI_DATA S
-ON 
-    F.PARENT_ID = S.ID
+# SELECT 
+#     F.ID
+# FROM 
+#     ECOLI_DATA F
+# JOIN 
+#     ECOLI_DATA S
+# ON 
+#     F.PARENT_ID = S.ID
+# JOIN
+#     ECOLI_DATA T
+# ON 
+#     S.PARENT_ID = T.ID
+# WHERE
+#     T.PARENT_ID IS NULL
+# ORDER BY
+#     F.ID
+
+# SELECT
+#     *
+# FROM
+#     ECOLI_DATA AS g1
+# JOIN
+#     ECOLI_DATA AS g2
+# ON
+#     g1.PARENT_ID = g2.ID
+# JOIN
+#     ECOLI_DATA AS g3
+# ON
+#     g2.PARENT_ID = g3.ID
+
+
+
+SELECT
+    g3.ID
+FROM
+    ECOLI_DATA AS g1
 JOIN
-    ECOLI_DATA T
-ON 
-    S.PARENT_ID = T.ID
+    ECOLI_DATA AS g2
+ON
+    g1.ID = g2.PARENT_ID
+JOIN
+    ECOLI_DATA AS g3
+ON
+    g2.ID = g3.PARENT_ID
 WHERE
-    T.PARENT_ID IS NULL
+    g1.PARENT_ID IS NULL
 ORDER BY
-    F.ID
+    g3.ID
+    
